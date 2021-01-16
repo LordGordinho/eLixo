86563621f627cc0b75326a134962dc897f47a3e5
Jorge Felipe <jfss1@ifal.edu.br>
2021-01-16 03:10:58 -0300

    Tela incial feita

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function telaInicial(){
+    setTimeout(carregaMenu, 1500)
+}
+
+function carregaMenu(){
+    let telaCarregamento = document.querySelector(".telaCarregamento")
+    telaCarregamento.style.display = "none"
+}
